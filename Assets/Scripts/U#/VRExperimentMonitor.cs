ab468ef6c1a363d246cdea4adac2c0246c1dd55c
MKyosei2 <matsukyo958@gmail.com>
2025-09-05 14:59:34 +0900

    Day36Var2

--- Assets/Scripts/U#/VRExperimentMonitor.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public class VRExperimentMonitor : MonoBehaviour
-{
-    public void Log(string msg) { Debug.Log("[Monitor] " + msg); }
-    public void LogWarning(string msg) { Debug.LogWarning("[Monitor] " + msg); }
-    public void LogError(string msg) { Debug.LogError("[Monitor] " + msg); }
-}
--- Assets/Scripts/U#/VisualExperimentPlayer.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public class VisualExperimentPlayer : MonoBehaviour
-{
-    public void PlayStart(SelectedObjectHolder sel) { Debug.Log("[Visual] Start"); }
-    public void PlayMessage(string message) { Debug.Log("[Visual] " + message); }
-    public void PlayFallback() { Debug.Log("[Visual] Fallback"); }
-}
